bf5694151c88b716cf6d98e3bc87e66567620a31
Ngbcoder <112007432+Ngbcoder@users.noreply.github.com>
2024-03-04 19:52:39 -0500

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -2,3 +2,10 @@ echo installing flathub
 sudo apt update
 sudo apt install flatpak
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+echo installing VNC viewer
+
+
+wget https://github.com/Ngbcoder/linux-app/blob/main/VNC-Viewer-7.10.0-Linux-x64.deb
+sudo dpkg VNC-Viewer-7.10.0-Linux-x64.deb
+
+flatpak install flathub org.prismlauncher.PrismLauncher
