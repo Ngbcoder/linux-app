15054fe3b1cd8315725001212f9c9ff93ae30535
Ngbcoder <112007432+Ngbcoder@users.noreply.github.com>
2024-09-23 20:32:05 -0400

    Update install.bat

--- windows/install.bat
+++ windows/install.bat
@@ -1,1 +1,2 @@
-
+echo installing terminal eartrumpet npp operaGX
+winget install --id=Microsoft.WindowsTerminal -e  && winget install --id=File-New-Project.EarTrumpet -e  && winget install --id=Notepad++.Notepad++ -e  && winget install --id=Opera.OperaGX -e
